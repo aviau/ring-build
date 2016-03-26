63d23578c391ca22f1c6c0f1dbc91b2c676941d9
aviau <alexandre@alexandreviau.net>
2016-03-25 22:09:34 -0400

    Improvements to make_tarball.sh

--- make_tarball.sh
+++ make_tarball.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
-TOP=`pwd`
-git clone --recursive https://github.com/savoirfairelinux/ring-project.git
-cd $TOP/daemon && git pull origin master
-cd $TOP/lrc && git pull origin master
-cd $TOP/client-gnome && git pull origin master
+currentdir=`pwd`
+tmp=`mktemp -d`
 
-cd $TOP
-tar --exclude-vcs -zcvf ring_20170301.0.git5cc051d.tar.gz ring-project
+cd $tmp
+
+# Download
+git clone --recursive https://github.com/savoirfairelinux/ring-project.git
+
+# Update repositories
+# TODO: remove this, ring-project should be up-to-date
+cd $tmp/ring-project/daemon && git pull origin master
+cd $tmp/ring-project/lrc && git pull origin master
+cd $tmp/ring-project/client-gnome && git pull origin master
+
+# Download contrib tarballs
+cd $tmp/ring-project/daemon/contrib
+mkdir native
+cd native
+../bootstrap
+make fetch
+cd ..
+rm -rf native
+
+# Create tarball
+cd $tmp
+tar --exclude-vcs -zcf ring_20170301.0.git5cc051d.tar.gz ring-project
 rm -rf ring-project
+
+# Move tarball to original dir
+mv $tmp/*.tar.gz $currentdir
+rm -rf $tmp
