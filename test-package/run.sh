90f982c12e017649dd8f46daf0c01b8a9a0b4b2a
aviau <alexandre@alexandreviau.net>
2016-04-22 23:10:37 -0400

    added tip

--- test-package/run.sh
+++ test-package/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-docker kill ring-test-package
-docker rm ring-test-package
+# you may need to run `xhost local:root`
 
-docker run -it \
+docker run -it --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd)/$1:/tmp/ring.deb:ro \
     --device /dev/snd \
     --device /dev/video0 \
     -e DISPLAY=unix$DISPLAY \
-    --name ring-test-package \
     debian:unstable \
     /bin/bash -c "dpkg -i /tmp/ring.deb; \
                   apt-get update && \
