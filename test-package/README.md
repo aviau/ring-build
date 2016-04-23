891758488d4307e6066aec93e5618e48abefc1a4
aviau <alexandre@alexandreviau.net>
2016-04-22 22:35:37 -0400

    added test-package script

---
+++ test-package/README.md
@@ -0,0 +1,5 @@
+# test-package
+
+This is used to test a debian package of ring inside docker
+
+usage: ./run.sh package.deb
---
+++ test-package/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker kill ring-test-package
+docker rm ring-test-package
+
+docker run -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $(pwd)/$1:/tmp/ring.deb:ro \
+    --device /dev/snd \
+    --device /dev/video0 \
+    -e DISPLAY=unix$DISPLAY \
+    --name ring-test-package \
+    debian:jessie \
+    /bin/bash -c "dpkg -i /tmp/ring.deb; \
+                  apt-get update && \
+                  apt-get -fy install && \
+                  gnome-ring"
